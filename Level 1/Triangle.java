463945fbcfb648f7801c5bcc9ce774cffd55afca
Maham-Suhail <63043652+Maham-Suhail@users.noreply.github.com>
2020-04-06 13:17:03 +0500

    Add files via upload

--- Level 1/Triangle.java
+++ Level 1/Triangle.java
@@ -1,20 +1,14 @@
 package com.company;
 
 import java.util.Scanner;
-// solution of quesion 4
+
 public class Triangle {
 
-    private int side1;
-    private int side2;
-    private int side3;
-    // constructor with parameters
-    public Triangle(int side1,int side2,int side3)
-    {
-        this.side1 = side1;
-        this.side2 = side2;
-        this.side3 = side3;
-    }
-    // calculate area of triangle
+    private int side1 = 3;
+    private int side2 = 4;
+    private int side3 = 5;
+
+
     public double area()
     {
         double s = perimeter()/2;
@@ -22,18 +16,20 @@ public class Triangle {
         double area = Math.sqrt(x);
         return area;
     }
-    //calculate area of perimeter
     public double perimeter()
     {
         double per = side1+side2+side3;
         return per;
     }
 
+
     public static void main(String[] args) {
 
-        Triangle tri = new Triangle(3,4,5);
+        Triangle tri = new Triangle();
         System.out.println("Area of triangle: "+tri.area());
         System.out.println("Perimeter of triangle: "+tri.perimeter());
+        
+
 
     }
 }

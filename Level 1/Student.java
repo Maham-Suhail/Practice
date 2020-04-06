a77c3a439552a08a978bc0c5c5d734323f7fa24d
Maham-Suhail <63043652+Maham-Suhail@users.noreply.github.com>
2020-04-06 13:24:13 +0500

    Student1

--- Level 1/Student.java
+++ Level 1/Student.java
@@ -1,18 +1,15 @@
 package com.company;
-// solution of 2nd question
-public class Student{
+// solution of first Problem
+public class Student {
 
     private String name;
     private int roll_no;
-    private String phone_no;
-    private String address;
 
-    Student(String name,int roll_no,String phone_no,String address)
+    // constructor initialize the student attributes
+    Student(String name,int roll_no)
     {
         this.name = name;
         this.roll_no = roll_no;
-        this.phone_no = phone_no;
-        this.address = address;
     }
 
     public String getName() {
@@ -23,23 +20,9 @@ public class Student{
         return roll_no;
     }
 
-    public String getPhone_no() {
-        return phone_no;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
     public static void main(String[] args) {
+        Student std = new Student("John", 2);
+        System.out.println("Name of Student is: "+std.name+"\n"+"Roll no of Student is: "+std.roll_no);
 
-	Student std1 = new Student("John",2,"03001234567","Lahore Pakistan");
-	Student std2 = new Student("Sam",3,"03001234567","Lahore Pakistan");
-
-	System.out.println("Name of Student: "+std1.name+"\n"+"Roll no: "+std1.roll_no+"\n"+
-            "Phone no: "+std1.phone_no+"\n"+"Address: "+std1.address+"\n");
-
-    System.out.println("Name of Student: "+std2.name+"\n"+"Roll no: "+std2.roll_no+"\n"+
-             "Phone no: "+std2.phone_no+"\n"+"Address: "+std2.address+"\n");
     }
 }
